8379eacfb5996761b513b6b5fd397ffe2a9db646
AllenChinese <13402202685@163.com>
2019-11-06 14:30:27 +0800

    feat: Run multiple commands concurrently

--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ var A = 1;
 [1, 2, 3].map(function (item) {
     console.log(item);
 });
+var B = 123;
+console.log(B);
+console.log('concurrently test successed, go go go!');
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ const A = 1
 ;[1, 2, 3].map(item => {
   console.log(item)
 })
+const B: number = 123
+console.log(B)
+
+console.log('concurrently test successed, go go go!')
